1e1e2ba3c1a232a11fc56064314b652cf45b340a
Yura <ualhovka@gmail.com>
2021-03-14 22:17:06 +0300

    adaptive carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left_solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right_solid.svg"></button>',
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left_solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right_solid.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
