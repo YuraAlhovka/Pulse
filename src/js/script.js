cbc2d11d4435f2472f0f4ca24336fc14b527a718
Yura <ualhovka@gmail.com>
2021-05-15 11:07:17 +0300

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -36,6 +36,20 @@ $(document).ready(function(){
 
   //Modal
 
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
   function validateForms(form){
     $(form).validate({
         rules: {
@@ -68,4 +82,31 @@ validateForms('#consultation form');
 validateForms('#order form');
 
 $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+$('form').submit(function(e){
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize()
+  }).done(function(){
+    $(this).find("input").val("");
+
+    $('form').trigger('reset');
+  });
+  return false;
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+  } else {
+      $('.pageup').fadeOut();
+  }
+});
+
+  $("a[href=#up]").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+  });
 });
